8bd30f2d9a929a90fcd727a1a8245c8e2c174556
Raj kothari <rkmeansrk39@gmail.com>
2020-05-12 01:48:57 +0530

    update

---
+++ app/src/main/java/com/example/nat_petgallery/Classes/ImageData.java
@@ -0,0 +1,25 @@
+package com.example.nat_petgallery.Classes;
+
+public class ImageData {
+    public String imgName;
+    public String imgUrl;
+
+    public ImageData() {
+    }
+
+    public String getImgName() {
+        return imgName;
+    }
+
+    public void setImgName(String imgName) {
+        this.imgName = imgName;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+}
