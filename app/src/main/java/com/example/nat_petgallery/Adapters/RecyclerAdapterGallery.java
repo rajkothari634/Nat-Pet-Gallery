67ada37b71f773d20fde50bc46e2d47cc03eb447
Raj kothari <rkmeansrk39@gmail.com>
2020-05-12 01:50:17 +0530

    update

--- app/src/main/java/com/example/nat_petgallery/Adapters/RecyclerAdapterGallery.java
+++ app/src/main/java/com/example/nat_petgallery/Adapters/RecyclerAdapterGallery.java
@@ -1,6 +1,7 @@
 package com.example.nat_petgallery.Adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +11,17 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.nat_petgallery.Classes.ImageData;
 import com.example.nat_petgallery.R;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 
 public class RecyclerAdapterGallery extends RecyclerView.Adapter<RecyclerAdapterGallery.ViewHolder> {
 
-    HashMap<String,String> imgAddress;
+    List<ImageData> imgAddress;
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -24,20 +29,30 @@ public class RecyclerAdapterGallery extends RecyclerView.Adapter<RecyclerAdapter
         return new ViewHolder(view);
     }
 
-    public RecyclerAdapterGallery(Context context,HashMap<String,String> imgAddress){
+    public RecyclerAdapterGallery(Context context,List<ImageData> imgAddress){
         this.imgAddress = imgAddress;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        
+        ImageData imageData = imgAddress.get(position);
+        holder.imgName.setText(imageData.getImgName());
+        holder.imgContainer.setImageDrawable(LoadImageFromWebOperations(imageData.getImgUrl()));
     }
 
     @Override
     public int getItemCount() {
         return imgAddress.size();
     }
-
+    public static Drawable LoadImageFromWebOperations(String url) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable drawable = Drawable.createFromStream(is, "src name");
+            return drawable;
+        } catch (Exception e) {
+            return null;
+        }
+    }
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public ImageView imgContainer;
